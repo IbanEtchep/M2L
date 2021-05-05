20b39d3397b5e24f0a16b1f218d5567d2e4f9c32
Iban <iban.etche@outlook.fr>
2021-05-05 11:54:01 +0200

    Objet intervenant

---
+++ models/Intervenant.php
@@ -0,0 +1,98 @@
+<?php
+class Intervenant
+{
+
+    private int $idI;
+    private string $nom;
+    private string $prenom;
+    private string $statut;
+    private string $email;
+    private string $mdp;
+    private string $fonction;
+
+
+    public function __construct(array $data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data){
+        foreach ($data as $key => $value){
+            $method = 'set'.ucfirst($key);
+            if(method_exists($this, $method)){
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function getId(): int
+    {
+        return $this->idI;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->idI = $id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function getStatut(): string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): void
+    {
+        $this->statut = $statut;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMdp(): string
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * @param string $mdp
+     */
+    public function setMdp(string $mdp): void
+    {
+        $this->mdp = $mdp;
+    }
+
+
+
+}
+
+?>
